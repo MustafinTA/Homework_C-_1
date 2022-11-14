a5211e0de7a154502557233d8ea0ada9bb788390
«TimurM» <Akshishma@mail.ru>
2022-11-14 22:13:14 +0300

    Задача №6

---
+++ task6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+    Console.WriteLine("Нечетное");
